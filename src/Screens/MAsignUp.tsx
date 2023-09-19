d736f01b04e82c06104121c43ab03c8602dc0c16
AlimEmon0333 <alimemon0333022@gmail.com>
2023-09-19 16:01:40 +0500

    Boiler Plate

--- src/Screens/MAsignUp.tsx
+++ src/Screens/MAsignUp.tsx
@@ -1,25 +1,59 @@
-import { Box, Paper, TextField, Typography, Button, Avatar } from "@mui/material";
+import { Box, Paper, TextField, Typography, Button, Avatar, Backdrop, CircularProgress } from "@mui/material";
 import { type } from "os";
 import MAinput from "../Components/MAinput";
 import MAbutton from "../Components/MAbutton";
 import PersonIcon from '@mui/icons-material/Person';
+import { useEffect, useState } from "react";
+// import { SignUp } from "../config/firebaseMethods";
+import { SignUp } from "../config/Router/firebaseMethods";
+import { useNavigate } from "react-router-dom";
+// import SimpleBackdrop from "../Components/MAloader";
 
-export default function MALogin() {
+export default function MASignUp() {
+    let [user, setUser] = useState<any>([])
+    const [open, setOpen] = useState(false);
+    const fillMOdel = (key: any, val: any) => {
+
+        user[key] = val;
+        setUser({ ...user })
+    }
+    let navigate = useNavigate()
+    const SignUpUser = () => {
+        console.log(user)
+        setOpen(true)
+        SignUp(user)  
+
+            .then((res: any) => {
+                navigate('/MADashboard')
+                setOpen(false)
+            }).catch((err: any) => {
+                alert(err)
+                setOpen(false)
+            })
+    }
     return (
         <>
             {/* Login */}
             <div className="flex justify-center items-center bg-gradient-to-r from-indigo-500 h-screen w-screen">
-                <div className="w-[350px] ease-in duration-300 bg-white p-5 rounded-lg border-t-4 border-indigo-500 border-b-4 border-indigo-500 hover:border-r-4 border-indigo-500 hover:border-l-4 border-indigo-500 hover:border-t-0 border-indigo-500 hover:border-b-0 border-indigo-500 flex justify-center items-center">
-
+                <div className="w-[400px] ease-in duration-300 bg-white p-5 rounded-lg border-t-4 border-indigo-500 border-b-4 border-indigo-500 hover:border-r-4 border-indigo-500 hover:border-l-4 border-indigo-500 hover:border-t-0 border-indigo-500 hover:border-b-0 border-indigo-500 flex justify-center items-center">
+                    {/* <SimpleBackdrop /> */}
                     <div className="py-2 flex-col flex justify-center items-center">
+
+                        <Backdrop
+                            sx={{ color: '#fff', zIndex: (theme) => theme.zIndex.drawer + 1 }}
+                            open={open}
+                            // onClick={handleClose}
+                        >
+                            <CircularProgress color="inherit" />
+                        </Backdrop>
                         <Avatar src="/broken-image.jpg" sx={{ width: "100px", height: "100px", backgroundColor: "transparent" }} className="block bg-gradient-to-r from-indigo-500 mb-4" />
-                        <MAinput label="User Name" type="text" className="w-100" />
+                        <MAinput label="User Name" type="text" onchange={(e: any) => fillMOdel("User Name", e.target.value)} />
                         <br />
-                        <MAinput label="Email" className="w-100" type="email" />
+                        <MAinput label="Email" type="email" onchange={(e: any) => fillMOdel("Email", e.target.value)} />
                         <br />
-                        <MAinput label="Password" className="w-100" type="password" />
+                        <MAinput label="Password" type="password" onchange={(e: any) => fillMOdel("Password", e.target.value)} />
                         <br />
-                        <MAbutton value="Sign Up" endIcon={<PersonIcon />} Classname="bg-gradient-to-r from-indigo-500 ease-linear duration-500 hover:bg-gradient-to-l from-indigo-300 w-75 h-50 py-2 rounded-lg" />
+                        <MAbutton value="Sign Up" Onclick={SignUpUser} endIcon={<PersonIcon />} Classname="bg-gradient-to-r from-indigo-500 ease-linear duration-500 hover:bg-gradient-to-l from-indigo-300 w-75 h-50 py-2 rounded-lg" />
                     </div>
                     <div className="py-5 block flex-col">
                     </div>
---
+++ src/config/Router/firebaseConfig.ts
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyAg2Oap_eWE08l5WqOzEGlkQ8BQwiII7S8",
+    authDomain: "react-app-firebase-intigration.firebaseapp.com",
+    projectId: "react-app-firebase-intigration",
+    storageBucket: "react-app-firebase-intigration.appspot.com",
+    messagingSenderId: "1061884547220",
+    appId: "1:1061884547220:web:54cc4d7cb9addd522c6481"
+};
+
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig);
